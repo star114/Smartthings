20951fc101b4f2be0d333a9c912ba02dbad8be1c
Heesik Shin <sinheesik@gmail.com>
2020-08-11 18:41:05 +0900

    Fixes #4 step 2 - Custom DTH: KuKu Harmony v2_Default (Patched)

---
+++ devicetypes/star114/kuku-harmony-default-patched.src/kuku-harmony-default-patched.groovy
@@ -0,0 +1,128 @@
+/**
+ *  KuKu Harmony v2 - Virtual Switch for Logitech Harmony (Patched)
+ *
+ *  Copyright 2017 KuKu <turlvo@gmail.com> / 2020 star114
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+metadata {
+    definition (name: "KuKu Harmony v2_Default", namespace: "star114", author: "KuKu/star114") {
+        capability "Actuator"
+        capability "Switch"
+        capability "Refresh"
+        capability "Sensor"
+        capability "Configuration"
+        capability "Health Check"
+        command "virtualOn"
+        command "virtualOff"
+
+        command "reboot"
+    }
+
+    tiles (scale: 2){
+        standardTile ("actionFlat", "device.switch", width: 2, height: 2, decoration: "flat") {
+            state "off", label: '${currentValue}', action: "switch.on", icon: "st.switches.switch.off", backgroundColor: "#ffffff", nextState:"turningOn"
+            state "on", label: '${currentValue}', action: "switch.off", icon: "st.switches.switch.on", backgroundColor: "#00a0dc", nextState:"turningOff"
+            state "off", label: '${currentValue}', action: "switch.on", icon: "st.switches.switch.off", backgroundColor: "#ffffff", nextState:"turningOn"
+            state "on", label: '${currentValue}', action: "switch.off", icon: "st.switches.switch.on", backgroundColor: "#00a0dc", nextState:"turningOff"
+        }
+    }
+
+    main(["switch"])
+    details(["switch"])
+}
+
+def installed() {
+    log.debug "installed()"
+    configure()
+}
+
+def configure() {
+    log.debug "configure()"
+}
+
+def updated() {
+    log.debug "updated()"
+}
+
+// parse events into attributes
+def parse(String description) {
+    log.debug "Parsing '${description}'"
+}
+
+def on() {
+    log.debug "child on"
+
+    log.debug "on>> ${device.currentState("switch")?.value}"
+    def currentState = device.currentState("switch")?.value
+
+    if (currentState == "on") {
+        log.debug "Already turned on, skip ON command"
+    } else {
+        parent.command(this, "power-on")
+        sendEvent(name: "switch", value: "on")
+    }
+}
+
+def off() {
+    log.debug "child off"
+
+    log.debug "off>> ${device.currentState("switch")?.value}"
+    def currentState = device.currentState("switch")?.value
+
+    if (currentState == "on") {
+        parent.command(this, "power-off")
+        sendEvent(name: "switch", value: "off")
+
+    } else {
+        log.debug "Already turned off, skip OFF command"
+    }
+}
+
+def virtualOn() {
+    log.debug "child on"
+    sendEvent(name: "switch", value: "on")
+}
+
+def virtualOff() {
+    log.debug "child off"
+    sendEvent(name: "switch", value: "off")
+}
+
+def generateEvent(Map results) {
+    results.each { name, value ->
+        log.debug "generateEvent>> name: $name, value: $value"
+        def currentState = device.currentValue("switch")
+        log.debug "generateEvent>> currentState: $currentState"
+        if (currentState != value) {
+            log.debug "generateEvent>> changed to $value"
+            sendEvent(name: "switch", value: value)
+        } else {
+            log.debug "generateEvent>> not change"
+        }
+    }
+    return null
+}
+
+def poll() {
+    log.debug "poll()"
+}
+
+def parseEventData(Map results) {
+    results.each { name, value ->
+        //Parse events and optionally create SmartThings events
+    }
+}
