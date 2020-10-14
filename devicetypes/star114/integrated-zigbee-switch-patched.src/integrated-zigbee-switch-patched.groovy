e824d2b71e803909e2e6460deebe9bdfbd26f004
Heesik Shin <sinheesik@gmail.com>
2020-10-14 22:02:43 +0900

    integrated-zigbee-switch cleanup history, impl uninstalled, do not install by model name

--- devicetypes/star114/integrated-zigbee-switch-patched.src/integrated-zigbee-switch-patched.groovy
+++ devicetypes/star114/integrated-zigbee-switch-patched.src/integrated-zigbee-switch-patched.groovy
@@ -17,94 +17,13 @@
  *
  *  author : woobooung@gmail.com / star114
  */
-public static String version() { return "v0.0.23.20200810" }
+public static String version() { return "v0.1.0" }
 /*
- *   2020/08/10 >>> v0.0.22.20200810 - modified by star114
- *   2020/06/19 >>> v0.0.22 - Add Terncy Switch
- *   2020/06/16 >>> v0.0.21 - When changed parent's dni, replace child's dni(select settings's "Auto detecting and create device" : ON)
- *   2020/06/14 >>> v0.0.20 - Added ZigBee 3.0 USB Socket Plug
- *   2020/06/14 >>> v0.0.19 - Auto detecting and create option
- *   2020/06/14 >>> v0.0.18 - Child device type change
- *   2020/06/14 >>> v0.0.17 - Fixed health check issue
- *   2020/06/14 >>> v0.0.16 - Support All Controller switch and fixed for Tuya usb switch offline issue
- *   2020/06/13 >>> v0.0.15 - Tuya multitab support USB
- *   2020/06/01 >>> v0.0.14 - Tuya multitab without USB
- *   2020/05/29 >>> v0.0.13 - Tuya multitab with USB
- *   2020/05/27 >>> v0.0.12 - Aqara 1gang switch mapping to ZigBee Switch Power
- *   2020/05/22 >>> v0.0.11 - Added Aqara switch 1gang
- *   2020/05/14 >>> v0.0.10 - Added Zemi1gang switch
- *   2020/05/08 >>> v0.0.9  - Modified for Zemi 2gang switch
- *   2020/04/27 >>> v0.0.8  - Added Usage
- *   2020/04/26 >>> v0.0.7  - Support to old Zemi swtich version
- *   2020/04/26 >>> v0.0.6  - Added Zemi swtich, and modified child device type name
- *   2020/04/25 >>> v0.0.5  - Fixed minor issue - child device lebel
- *   2020/04/25 >>> v0.0.4  - Fixed minor issue
- *   2020/04/25 >>> v0.0.4  - Modified Child Device Lebel
- *   2020/04/25 >>> v0.0.4  - Fixed minor bugs
- *   2020/04/25 >>> v0.0.3  - Added Goqual Multi Switch & Modified refresh() function
- *   2020/04/25 >>> v0.0.2  - Added : DAWON DNS ZigBee Multi Switch 1 2 3 gang,  eZex ZigBee Multi Switch 6 gang, old Zigbee OnOff Swtich
- *   2020/04/25 >>> v0.0.1  - Initialize : Bandi ZigBee Switch, Zemi ZigBee Switch
+ *   2020/10/14 >>> v0.1.0 - modified by star114
  */
 
 import java.lang.Math
 import groovy.json.JsonOutput
-
-/*
-    > Usage <
-
-    Example) Bandi 3gang switch model
-    IDE Device Data
-      manufacturer: _TYZB01_pdevogdj
-      model: TS0003
-
-      Raw Description	01 0104 0100 00 05 0000 000A 0004 0005 0006 01 0019
-        Analysis -> '01'(endpointId) '0104'(profileId) '0100'(deviceId) 00(skip) 05(skip) '0000 000A 0004 0005 0006'(inClusters) 01(skil) '0019'(outClusters)
-
-    > Step1 - Add fingerprint
-    fingerprint endpointId: "01", profileId: "0104", deviceId: "0100", inClusters: "0000, 000A, 0004, 0005, 0006", outClusters: "0019", manufacturer: "_TYZB01_pdevogdj", model: "TS0003", deviceJoinName: "Bandi Zigbee Switch 1"
-
-    > Step 2 - Add MODEL_MAP
-    private getMODEL_MAP() {
-        [
-            'TS0003' : 3,
-            ....
-            ...
-            ..
-        ]
-    }
-
-    > Step 3 - paring device and test
-
-    > Step 4 - request add device infos to woobooung@gmail.com
-*/
-
-private getMODEL_MAP() {
-    [
-        'TS0003' : 3,
-        'TS0002' : 2,
-        'TS0001' : 1,
-        'TS0115' : 4,
-        'TS0112' : 2,
-        'PM-S340-ZB' : 3,
-        'PM-S240-ZB' : 2,
-        'PM-S140-ZB' : 1,
-        'FNB56-ZSW03LX2.0' : 3,
-        'FNB56-ZSW02LX2.0' : 2,
-        'FNB56-ZSW01LX2.0' :1,
-        'FB56+ZSW1GKJ2.7' : 1,
-        'FB56+ZSW1HKJ2.5' : 2,
-        'FB56+ZSW1IKJ2.7' : 3,
-        'E220-KR6N0Z1-HA' : 6,
-        'Lamp_01' : 1,
-        'lumi.switch.b1naus01': 1,
-        'lumi.switch.b2naus01': 2,
-        'lumi.switch.b3naus01': 3,
-        'lumi.ctrl_neutral1' : 1,
-        'lumi.switch.b2laus01' : 2,
-        'TERNCY-WS01-S2' : 2,
-        'TERNCY-WS01-S3' : 3
-    ]
-}
 
 metadata {
     definition(name: "Integrated ZigBee Switch (Patched)", namespace: "star114", author: "Booung/star114", ocfDeviceType: "oic.d.switch", vid: "generic-switch") {
@@ -117,50 +36,8 @@ metadata {
         command "childOn", ["string"]
         command "childOff", ["string"]
 
-        // Bandi ZigBee Multi Switch
-        // fingerprint endpointId: "01", profileId: "0104", deviceId: "0100", inClusters: "0000, 000A, 0004, 0005, 0006", outClusters: "0019", manufacturer: "_TZ3000_pdevogdj", model: "TS0003", deviceJoinName: "Bandi ZigBee Switch 1"
-        // fingerprint endpointId: "01", profileId: "0104", deviceId: "0100", inClusters: "0000, 000A, 0004, 0005, 0006", outClusters: "0019", manufacturer: "_TZ3000_tas0zemd", model: "TS0002", deviceJoinName: "Bandi ZigBee Switch 1"
-        // fingerprint endpointId: "01", profileId: "0104", deviceId: "0100", inClusters: "0000, 000A, 0004, 0005, 0006", outClusters: "0019", manufacturer: "_TZ3000_ddg0cycp", model: "TS0001", deviceJoinName: "Bandi ZigBee Switch"
-        // fingerprint endpointId: "01", profileId: "0104", deviceId: "0100", inClusters: "0000, 000A, 0004, 0005, 0006", outClusters: "0019", manufacturer: "_TYZB01_pdevogdj", model: "TS0003", deviceJoinName: "Bandi ZigBee Switch 1"
-        // fingerprint endpointId: "01", profileId: "0104", deviceId: "0100", inClusters: "0000, 000A, 0004, 0005, 0006", outClusters: "0019", manufacturer: "_TYZB01_tas0zemd", model: "TS0002", deviceJoinName: "Bandi ZigBee Switch 1"
-        // fingerprint endpointId: "01", profileId: "0104", deviceId: "0100", inClusters: "0000, 000A, 0004, 0005, 0006", outClusters: "0019", manufacturer: "_TYZB01_ddg0cycp", model: "TS0001", deviceJoinName: "Bandi ZigBee Switch"
-
-        // DAWON DNS ZigBee Multi Switch
-        // fingerprint endpointId: "01", profileId: "0104", deviceId: "0103", inClusters: "0000, 0004, 0003, 0006, 0019, 0002, 0009", outClusters: "0000, 0004, 0003, 0006, 0019, 0002, 0009", manufacturer: "DAWON_DNS", model: "PM-S140-ZB", deviceJoinName: "DAWON ZigBee Switch"
-        // fingerprint endpointId: "01", profileId: "0104", deviceId: "0103", inClusters: "0000, 0004, 0003, 0006, 0019, 0002, 0009", outClusters: "0000, 0004, 0003, 0006, 0019, 0002, 0009", manufacturer: "DAWON_DNS", model: "PM-S240-ZB", deviceJoinName: "DAWON ZigBee Switch 1"
-        // fingerprint endpointId: "01", profileId: "0104", deviceId: "0103", inClusters: "0000, 0004, 0003, 0006, 0019, 0002, 0009", outClusters: "0000, 0004, 0003, 0006, 0019, 0002, 0009", manufacturer: "DAWON_DNS", model: "PM-S340-ZB", deviceJoinName: "DAWON ZigBee Switch 1"
-
-        // Zemi ZigBee Multi Switch
-        // fingerprint endpointId: "10", profileId: "0104", inClusters: "0000, 0003, 0004, 0005, 0006", manufacturer: "Feibit Inc co.", model: "FB56+ZSW1GKJ2.7", deviceJoinName: "Zemi ZigBee Switch"
-        // fingerprint endpointId: "10", profileId: "0104", deviceId: "0002", inClusters: "0000, 0005, 0004, 0006", outClusters: "0000", manufacturer: "Feibit Inc co.", model: "FB56+ZSW1HKJ2.5", deviceJoinName: "Zemi ZigBee Switch 1"
-        // fingerprint endpointId: "10", profileId: "0104", deviceId: "0002", inClusters: "0000, 0003, 0004, 0005, 0006", manufacturer: "Feibit Inc co.", model: "FB56+ZSW1IKJ2.7", deviceJoinName: "Zemi ZigBee Switch 1"
-        // fingerprint endpointId: "0B", profileId: "C05E", inClusters: "0000, 0004, 0003, 0006, 0005, 1000, 0008", outClusters: "0019", manufacturer: "FeiBit", model: "FNB56-ZSW01LX2.0", deviceJoinName: "Zemi ZigBee Switch"
-        // fingerprint endpointId: "0B", profileId: "C05E", inClusters: "0000, 0004, 0003, 0006, 0005, 1000, 0008", outClusters: "0019", manufacturer: "FeiBit", model: "FNB56-ZSW02LX2.0", deviceJoinName: "Zemi ZigBee Switch 1"
-        // fingerprint endpointId: "01", profileId: "C05E", inClusters: "0000, 0004, 0003, 0006, 0005, 1000, 0008", outClusters: "0019", manufacturer: "FeiBit", model: "FNB56-ZSW03LX2.0", deviceJoinName: "Zemi ZigBee Switch 1"
-
-        // eZex ZigBee Multi Switch
-        // fingerprint endpointId: "01", profileId: "0104", deviceId: "0100", inClusters: "0000 0003 0004 0006", outClusters: "0006, 000A, 0019", manufacturer: "", model: "E220-KR6N0Z1-HA", deviceJoinName: "eZex ZigBee Switch 1"
-
-        // Aqara Switch
-        // fingerprint endpointId: "01", profileId: "0104", deviceId: "0100", inClusters: "0000, 0002, 0003, 0004, 0005, 0006, 0009, 0702, 0B04", outClusters: "000A, 0019", manufacturer: "LUMI", model: "lumi.switch.b1naus01", deviceJoinName: "Aqara Switch"
-        // fingerprint endpointId: "01", profileId: "0104", deviceId: "0100", inClusters: "0000, 0002, 0003, 0004, 0005, 0006, 0009, 0702, 0B04", outClusters: "000A, 0019", manufacturer: "LUMI", model: "lumi.switch.b2naus01", deviceJoinName: "Aqara Switch 1"
-        // fingerprint endpointId: "01", profileId: "0104", deviceId: "0100", inClusters: "0000, 0002, 0003, 0004, 0005, 0006, 0009, 0702, 0B04", outClusters: "000A, 0019", manufacturer: "LUMI", model: "lumi.switch.b3naus01", deviceJoinName: "Aqara Switch 1"
-        // fingerprint endpointId: "01", profileId: "0104", deviceId: "0100", inClusters: "0000, 0002, 0003, 0004, 0005, 0006, 0009", outClusters: "000A, 0019", manufacturer: "LUMI", model: "lumi.switch.b2laus01", deviceJoinName: "Aqara Switch 1"
-        // fingerprint endpointId: "01", profileId: "0104", deviceId: "0006", inClusters: "0000, 0003, 0001, 0002, 0019, 000A", outClusters: "0000, 000A, 0019", manufacturer: "LUMI", model: "lumi.ctrl_neutral1", deviceJoinName: "Aqara Switch"
-
-        // Zigbee OnOff Swtich
-        // fingerprint endpointId: "0B", profileId: "0104", deviceId: "0100", inClusters: "0000, 0003, 0004, 0005, 0006", outClusters: "0000", manufacturer: "SZ", model: "Lamp_01", deviceJoinName: "Zigbee OnOff Switch"
-
-        // Tuya multitab with USB
-        // fingerprint endpointId: "01", profileId: "0104", deviceId: "0009", inClusters: "0000, 000A, 0004, 0005, 0006", outClusters: "0019", manufacturer: "_TYZB01_vkwryfdr", model: "TS0115", deviceJoinName: "Tuya Multi Tab Switch 1"
-        // fingerprint endpointId: "01", profileId: "0104", deviceId: "0051", inClusters: "0000, 0003, 0004, 0005, 0006, 0702, 0B04", outClusters: "0019", manufacturer: "_TYZB01_b1ngbmlm", model: "TS0112", deviceJoinName: "Tuya USB Socket Plug 1"
-
-        // Terncy Switch
-        // fingerprint endpointId: "01", profileId: "0104", deviceId: "0100", inClusters: "0000, 0003, 0006, 0020, FCCC", outClusters: "0019", manufacturer: "Terncy", model: "TERNCY-WS01-S3", deviceJoinName: "Terncy Switch 1"
-        // fingerprint endpointId: "01", profileId: "0104", deviceId: "0100", inClusters: "0000, 0003, 0006, 0020, FCCC", outClusters: "0019", manufacturer: "Terncy", model: "TERNCY-WS01-S2", deviceJoinName: "Terncy Switch 1"
-
         // Unclear devices without model, manufacturer
-        // Hej-Home GQ 3 way Switch (no model and manufacturer
+        // Hej-Home GQ 3 way Switch (no model and manufacturer)
         fingerprint endpointId: "01", profileId: "0104", deviceId: "0100", inClusters: "0006, 0000, 0003", outClusters: "0019", manufacturer: "", model: "", deviceJoinName: "ZigBee Switch 1"
     }
 
@@ -200,22 +77,6 @@ metadata {
 
 def installed() {
     log.debug "installed()"
-    def endpointCount = getEndpointCount()
-    def model = device.getDataValue("model")
-    if (endpointCount == 1) {
-        // for 1 gang switch - ST Official local dth
-        // if (model == 'lumi.switch.b1naus01') {
-        //    setDeviceType("ZigBee Switch Power")
-        // } else {
-        setDeviceType("ZigBee Switch")
-        // }
-    } else if (endpointCount > 1){
-        // if (model == 'FB56+ZSW1HKJ2.5' || model == 'FB56+ZSW1IKJ2.7') {
-        //     device.updateDataValue("endpointId", "10")
-        // }
-        // for multi switch, cloud device
-        createChildDevices()
-    }
     updateDataValue("onOff", "catchall")
     state.hasConfiguredHealthCheck = false
     refresh()
@@ -226,6 +87,18 @@ def updated() {
     updateDataValue("onOff", "catchall")
     state.hasConfiguredHealthCheck = false
     refresh()
+}
+
+def uninstalled() {
+    log.debug "uninstalled()"
+    unschedule()
+    removeChildDevices(childDevices())
+}
+
+private removeChildDevices(delete) {
+    delete.each {
+        deleteChildDevice(it.deviceNetworkId)
+    }
 }
 
 def parse(String description) {
@@ -321,27 +194,6 @@ private getEndpointCount() {
 
     log.debug("getEndpointCount[$model] : $count")
     return count
-}
-
-private void createChildDevices() {
-    log.debug("=============createChildDevices of $device.deviceNetworkId")
-    if (!state.isCreateChildDone || isAutoCreateChildDevice != false) {
-        def endpointCount = getEndpointCount()
-        def endpointId = device.getDataValue("endpointId")
-        def endpointInt = zigbee.convertHexToInt(endpointId)
-        def deviceLabel = "${device.displayName[0..-2]}"
-
-        for (i in 1..endpointCount - 1) {
-            def endpointHexString = zigbee.convertToHexString(endpointInt + i, 2).toUpperCase()
-            createChildDevice("$deviceLabel${i + 1}", endpointHexString)
-        }
-
-        def model = device.getDataValue("model")
-        // if ( model == 'TS0115') {
-        //    createChildDevice("${deviceLabel}USB", "07")
-        // }
-        state.isCreateChildDone = true
-    }
 }
 
 private void createChildDevice(String deviceLabel, String endpointHexString) {
